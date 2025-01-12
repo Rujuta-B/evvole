21ace8a3720d144469a35c3e4724a2f5e6b5b018
Rujuta Batavia <rujuta.b@simformsolutions.com>
2025-01-12 19:53:13 +0530

    make different components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import './App.css';
-import Home from './components/Home';
+
+// import './App.css'
+import Home from './components/Home'
 
 function App() {
+
+
   return (
     <div className="App">
-      <Home />
-    </div>
-  );
+    <Home />
+  </div>
+  )
 }
 
-export default App;
+export default App
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,25 +1,30 @@
-import '../styles/home.css';
-import logoV from '../assets/logo-v.svg';
+import '../style/home.css'
+import logoV from '../assets/logoV.svg';
 import instagramIcon from '../assets/instagram-icon.svg';
-import logoName from '../assets/logo-name.svg';
+import logoName from '../assets/logoName.svg';
 
 const Home = () => {
   return (
     <div className="full-screen-background">
-        <img src={logoV} alt="logo-v" />
-        <h1>LAUNCHING <br />SOON!</h1>
-        <p>We are currently making some <br />
-          improvements to our website!</p>
-        <button>NOTIFY ME</button>
-        <a
-          href='https://www.instagram.com/evvole_exims?igsh=MTEwbmJma25vdm1wMA=='
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img className="insta-icon" src={instagramIcon} alt="instagram" />
-        </a>
-        <img className="name-logo" src={logoName} alt="logo-name" />
+      <img className="logo-v" src={logoV} alt="logo-v" />
+      <div className="launching-text">
+        <h1>LAUNCHING</h1>
+        <h1>SOON!</h1>
       </div>
+
+      <p>We are currently making some <br />
+        improvements to our website!</p>
+      <button onClick={() => console.log('first')}>NOTIFY ME</button>
+      <a
+        href='https://www.instagram.com/evvole_exims?igsh=MTEwbmJma25vdm1wMA=='
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {/* <h6>For more contenct checkout our Instagram</h6> */}
+        <img className="insta-icon" src={instagramIcon} alt="instagram" />
+      </a>
+      <img className="name-logo" src={logoName} alt="logo-name" />
+    </div>
   )
 }
 
